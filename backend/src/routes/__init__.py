23010e74c1281b982cfa07aad0d4f0c498f1b1f3
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2025-10-26 17:52:12 -0300

    use data instead of pipeline

--- backend/src/routes/__init__.py
+++ backend/src/routes/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from src.routes import weekly, pipeline, storage
+from src.routes import weekly, data, storage
 
 router = APIRouter()
 
 router.include_router(weekly.router, prefix="/weekly")
-router.include_router(pipeline.router, prefix="/pipeline")
+router.include_router(data.router, prefix="/data")
 router.include_router(storage.router, prefix="/storage")
