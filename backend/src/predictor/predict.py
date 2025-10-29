d8c518d9fd19112dae908233f36c2f81c3e4f3c4
MallkuCh <mmchipana@uc.cl>
2025-10-29 19:17:16 -0300

    deat: functions to clean code endpoit prediciton

--- backend/src/predictor/predict.py
+++ backend/src/predictor/predict.py
@@ -4,6 +4,58 @@ import numpy as np
 import prophet
 import joblib
 
+def pre_process_X_pred(df: pd.DataFrame):
+    X = df.drop(columns=["demanda_pacientes", "complejidad"])
+    X['año'] = X['semana_año'].str.split('-').str[0].astype(int)
+    X['semana'] = X['semana_año'].str.split('-').str[1].astype(int)
+    X['semana_continua'] = X['año'] + X['semana'] / 100
+    X = X.drop(columns=['semana_año'])
+    X = X.select_dtypes(exclude=['datetime64[ns]'])
+    return X
+
+def predict_prophet_model(model, periods: int = 1):
+    """
+    Realiza una predicción utilizando un modelo Prophet.
+    
+    Args:
+        model: Modelo Prophet entrenado.
+        periods: Número de períodos futuros a predecir.
+        
+    Returns:
+        DataFrame con las predicciones.
+    """
+    future = model.make_future_dataframe(periods=periods, freq='W')
+    forecast = model.predict(future)
+    return forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(periods)
+
+def predict_random_forest(model, X_pred):
+    """
+    Realiza una predicción utilizando un modelo Random Forest.
+    
+    Args:
+        model: Modelo Random Forest entrenado.
+        X_pred: DataFrame con las características para la predicción.
+        
+    Returns:
+        Diccionario con la predicción y el intervalo de confianza.
+    """
+    y_pred = model.predict(X_pred)
+
+    tree_preds = np.array([tree.predict(X_pred) for tree in model.estimators_])  # matriz (n_trees, n_muestras)
+
+    preds_ultimo = tree_preds[:, -1]
+
+    mean_pred = np.mean(preds_ultimo)
+    std_pred = np.std(preds_ultimo)
+
+    lower = mean_pred - 1.96 * std_pred
+    upper = mean_pred + 1.96 * std_pred
+
+    return {
+        "prediccion": float(y_pred[-1]),
+        "intervalo_confianza": [lower, upper]
+    }
+
 def predict(complexity: str):
     """
     Realiza una predicción para una complejidad específica.
@@ -11,134 +63,56 @@ def predict(complexity: str):
     Args:
         complexity: Nombre de la complejidad (Alta, Media, Baja, Neonatología, Pediatría)
     """
-    # datos = {
-    # "alta": {
-    #     "demanda_pacientes": 50,
-    #     "estancia (días)": 5.2,
-    #     "tipo de paciente_No Qx": 0.6,
-    #     "tipo de paciente Qx": 0.4,
-    #     "tipo de ingreso_No Urgente": 0.7,
-    #     "tipo de ingreso_Urgente": 0.3,
-    #     "fecha ingreso completa": "2024-10-20"
-    # },
-    # "baja": {
-    #     "demanda_pacientes": 30,
-    #     "estancia (días)": 3.5,
-    #     "tipo de paciente_No Qx": 0.8,
-    #     "tipo de paciente Qx": 0.2,
-    #     "tipo de ingreso_No Urgente": 0.85,
-    #     "tipo de ingreso_Urgente": 0.15,
-    #     "fecha ingreso completa": "2024-10-20"
-    # },
-    # "media": {
-    #     "demanda_pacientes": 40,
-    #     "estancia (días)": 4.5,
-    #     "tipo de paciente_No Qx": 0.7,
-    #     "tipo de paciente Qx": 0.3,
-    #     "tipo de ingreso_No Urgente": 0.75,
-    #     "tipo de ingreso_Urgente": 0.25,
-    #     "fecha ingreso completa": "2024-10-20"
-    # },
-    # "neonatología": {
-    #     "demanda_pacientes": 15,
-    #     "estancia (días)": 8,
-    #     "tipo de paciente_No Qx": 0.9,
-    #     "tipo de paciente Qx": 0.1,
-    #     "tipo de ingreso_No Urgente": 0.5,
-    #     "tipo de ingreso_Urgente": 0.5,
-    #     "fecha ingreso completa": "2024-10-20"
-    # },
-    # "pediatría": {
-    #     "demanda_pacientes": 25,
-    #     "estancia (días)": 4,
-    #     "tipo de paciente_No Qx": 0.75,
-    #     "tipo de paciente Qx": 0.25,
-    #     "tipo de ingreso_No Urgente": 0.6,
-    #     "tipo de ingreso_Urgente": 0.4,
-    #     "fecha ingreso completa": "2024-10-20"
-    # }
-    # }
 
-    # df_pred = preparar_datos_prediccion_global(datos)
-    # print(df_pred.head())
     BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
     ## Cargar datos desde CSV (CAMBIABLE)
     
-    DATA_PATH = BASE_DIR / "data" / "datos_prediccion_semanal.csv"
+    DATA_PATH = BASE_DIR / "data" / "datos_prediccion_semanal.csv" # cambiar a prediccion.csv en prod
 
     data_total = pd.read_csv(DATA_PATH)
     complexity = complexity.lower()
     df = data_total[data_total["complejidad"] == complexity]
     print(df.head())
+    try:
+        model_path = BASE_DIR / "models" / f"model_{complexity}.pkl"
+        np.random.seed(42)
+        model = joblib.load(model_path)
+    except Exception as e:
+        print(f"modelo en formato no .pkl... probando json")
+        model_path = BASE_DIR / "models" / f"model_{complexity}.json"
+        model = joblib.load(model_path)
 
-    ## Cargar el modelo entrenado
-    model_path = BASE_DIR / "models" / f"model_{complexity}.pkl"
-    np.random.seed(42)
-    model = joblib.load(model_path)
-    print(f"Modelo cargado desde {model_path}")
-    
     ## Realizar la predicción
     if complexity == "baja":
         print("Usando modelo Prophet")
-        future = model.make_future_dataframe(periods=1, freq='W')
-
-        forecast = model.predict(future)
-
-        forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(12)
-        print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(1))
+        result = predict_prophet_model(model, periods=1)
+        print(result)
+        return {"prediccion": result.to_dict(orient='records')}
 
     elif complexity == "media":
         print("🌲 Usando modelo RandomForest")
         print(df.head())
         print(df.columns)
-        X_pred = df.drop(columns=["demanda_pacientes", "complejidad"])
-
-        X_pred['año'] = X_pred['semana_año'].str.split('-').str[0].astype(int)
-        X_pred['semana'] = X_pred['semana_año'].str.split('-').str[1].astype(int)
-
-        X_pred['semana_continua'] = X_pred['año'] + X_pred['semana'] / 100
-
-        X_pred = X_pred.drop(columns=['semana_año'])
-        X_pred = X_pred.select_dtypes(exclude=['datetime64[ns]'])
-
-        y_pred = model.predict(X_pred)
-        result = {
-            "modelo": "RandomForest",
-            "prediccion": float(y_pred[-1]),
-        }
-        print(result)
+        X_pred = pre_process_X_pred(df)
+        result = predict_random_forest(model, X_pred)
+        return result
     elif complexity == "alta":
         print("Usando modelo Random Forest")
         print(df.head())
         print(df.columns)
-        X_pred = df.drop(columns=["demanda_pacientes", "complejidad"])
-
-        X_pred['año'] = X_pred['semana_año'].str.split('-').str[0].astype(int)
-        X_pred['semana'] = X_pred['semana_año'].str.split('-').str[1].astype(int)
-
-        X_pred['semana_continua'] = X_pred['año'] + X_pred['semana'] / 100
-
-        X_pred = X_pred.drop(columns=['semana_año'])
-        X_pred = X_pred.select_dtypes(exclude=['datetime64[ns]'])
-
-        y_pred = model.predict(X_pred)
-        print(f"Predicción: {y_pred[-1]}")
-
+        X_pred = pre_process_X_pred(df)
+        result = predict_random_forest(model, X_pred)
+        return result
     elif complexity == "neonatologia":
-        future = model.make_future_dataframe(periods=1, freq='W')
+        result = predict_prophet_model(model, periods=1)
+        print(result)
+        return {"prediccion": result.to_dict(orient='records')}
 
-        forecast = model.predict(future)
-
-        forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(12)
-        print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(1))
     elif complexity == "pediatria":
-        future = model.make_future_dataframe(periods=1, freq='W')
-
-        forecast = model.predict(future)
-
-        forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(12)
-        print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(1))
+        result = predict_prophet_model(model, periods=1)
+        print(result)
+        return {"prediccion": result.to_dict(orient='records')}
     
     return {"message": "Predicción realizada correctamente", "complexity": complexity}
 
