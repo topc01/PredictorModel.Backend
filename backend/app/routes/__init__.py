954dba580ef4d502596554423851f9c845cd63d8
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2025-11-20 15:54:39 -0300

    include pipeline and users endpoints

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.routes import weekly, data, storage, predict
+from app.routes import weekly, data, storage, predict, users, pipeline
 
 router = APIRouter()
 
@@ -8,3 +8,5 @@ router.include_router(weekly.router, prefix="/weekly")
 router.include_router(data.router, prefix="/data")
 router.include_router(storage.router, prefix="/storage")
 router.include_router(predict.router, prefix="/predict")
+router.include_router(users.router, prefix="/users")
+router.include_router(pipeline.router, prefix="/pipeline")
